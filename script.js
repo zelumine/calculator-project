7301974fd2a3d97b6d172b6bb5e5e5b5dc579a13
Melusine <zelumine@live.fr>
2019-07-03 10:58:07 +0200

    changed a few things and fixed the display (for now)

--- script.js
+++ script.js
@@ -1,73 +1,78 @@
     // Calculator functions 
-function add(a, b) {
-	return a + b;
+function add(arr) {
+	return arr.reduce((a, b) => a + b);
 }
 
-function subtract(a, b) {
-	return a - b;
+function subtract(arr) {
+	return arr.reduce((a, b) => a - b);
 }
 
-function divide(a, b) {
-    return a / b;
+function divide(arr) {
+    return arr.reduce((a, b) => a / b);
 }
 
-function multiply(arguments) {
- 	return arguments.reduce((a, b) => a * b);
+function multiply(arr) {
+ 	return arr.reduce((a, b) => a * b);
 }
 
-function operate(operator, a, b) {
+function operate(operator, arr) {
     switch(operator) {
         case "+":
-            console.log(add(a, b))
-            return add(a, b);
+            console.log(add(arr))
+            return add(arr);
             break;
         case "-":
-            console.log(subtract(a, b));
-            return subtract(a, b);
+            console.log(subtract(arr));
+            return subtract(arr);
             break;
         case "x":
-            console.log(multiply(a, b));
-            return multiply(a, b);
+            console.log(multiply(arr));
+            return multiply(arr);
             break;
         case "/":
-            console.log(divide(a, b));
-            return divide(a, b);
+            console.log(divide(arr));
+            return divide(arr);
             break;
     }
-}
+} 
     //End calculator functions
 
 let calc = document.getElementById("calculator");
 let buttons = Array.from(document.getElementsByTagName("button"));
-let display = document.getElementById("display");
+let displayArea = document.getElementById("display");
 let numbersAndOperators = [];
 let numbersArr = [];
-let operatorsArr = [];
 let num;
 let displayText = document.createElement("p");
+let resultOperator = document.getElementById("result");
+let result;
+let resultArr = [];
 
 buttons.forEach( (button) => {
-    button.addEventListener("click", (e) => {
-        if(e.target.className === "operator") {
-            numbersArr.push("");
-            num = numbersArr.join("");
-            numbersArr = [];
-            console.log(numbersArr)
-            console.log("num : " + num)
-            numbersAndOperators.push(num);
-            numbersAndOperators.push(e.target.textContent);
-            //displayText.textContent = numbersAndOperators.join(" "); 
-        } else if (e.target.className === "number") {
-            numbersArr.push(e.target.textContent);
-            displayText.textContent = numbersAndOperators.join(" "); 
-        }
-        console.log(numbersAndOperators);
-        displayText.classList.add("display-items");
-        display.appendChild(displayText);
-    });
+    button.addEventListener("click", displayFunc);
 }); 
 
 
+function displayFunc(e) {
+    displayArea.textContent = "";
+    if(e.target.className === "operator") {
+        displayText.textContent += " " + e.target.textContent + " ";
+        num = numbersArr.join("");
+        resultArr.push(parseInt(num, 10));
+        numbersArr = [];
+        numbersAndOperators.push(parseInt(num, 10));
+        numbersAndOperators.push(e.target.textContent);
+    } else if (e.target.className === "number") {
+        numbersArr.push(e.target.textContent);
+        displayText.textContent += e.target.textContent;
+    }
+    display.appendChild(displayText);
+}
 
+// For now it works when it's the same operator
+resultOperator.addEventListener("click", (e) => {
+    result = operate(numbersAndOperators[1], resultArr);
+    displayText.textContent = result;
+});
 
 
